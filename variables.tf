7c9d374b741b4a353bc96e776402081903c6bbf5
Suganthan <105227226+sugi2612@users.noreply.github.com>
2026-07-13 08:33:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,35 @@
-terraform {
-  required_version = ">= 1.6.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "microservices-demo"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "db_name" {
+  description = "PostgreSQL database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "PostgreSQL username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "PostgreSQL password"
+  type        = string
+  sensitive   = true
 }
